b0d2bbbca66b5177486b0227946f1b24c4d7ab9f
Max Liberti <maxliberti@gmail.com>
2025-04-03 14:56:19 -0400

    changed organization and made variables for CHIP8 components

--- cpu.py
+++
@@ -1,7 +0,0 @@
-class CPU:
-    def __init__(self):
-        self.memory = bytearray(4096) # 4kb of RAM
-        self.registers = [0x00] * 16 # 16 register slots
-
-cpu = CPU()
-
--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import pygame
 
-screen = pygame.display.set_mode((64, 32))
-pygame.display.set_caption("CHIP 8")
-screen.fill((255,255,255))
-pygame.display.flip()
+class CHIP8:
+    def __init__(self):
+        self.memory = [0x000] * 4096
+        self.registers = [0x00] * 16
+        self.registers[0xF] = False # VF
+        self.stack = [0] * 16
+        self.pc = 0
+        self.delayTimer = 0
+        self.soundTimer = 0
+        self.keypad = [0] * 16
+        self.video = [ [0]*64 for i in range(32) ]
+        self.opcode = 0
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+
+
+
+
+
+def main():
+    screen = pygame.display.set_mode((64, 32))
+    pygame.display.set_caption("CHIP 8")
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    chip8 = CHIP8()
+    print(chip8.registers)
+
+if __name__ == "__main__":
+    main()
 
